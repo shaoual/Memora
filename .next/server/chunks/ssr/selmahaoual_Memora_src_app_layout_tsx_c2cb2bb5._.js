767442a83183bac0c4cfff3503e4601e785f1da1
shaoual <127392211+shaoual@users.noreply.github.com>
2026-02-04 17:54:56 +0100

    ajout contenu + animations

---
+++ .next/server/chunks/ssr/selmahaoual_Memora_src_app_layout_tsx_c2cb2bb5._.js
@@ -0,0 +1,38 @@
+module.exports = {
+
+"[project]/selmahaoual/Memora/src/app/layout.tsx [app-rsc] (ecmascript)": (function(__turbopack_context__) {
+
+var { g: global, __dirname, m: module, e: exports } = __turbopack_context__;
+{
+const e = new Error(`Could not parse module '[project]/selmahaoual/Memora/src/app/layout.tsx'
+
+creating new process
+
+Caused by:
+- node process exited before we could connect to it with exit status: 1
+
+Debug info:
+- Execution of <WebpackLoadersProcessedAsset as Asset>::content failed
+- Execution of WebpackLoadersProcessedAsset::process failed
+- creating new process
+- node process exited before we could connect to it with exit status: 1
+  Process output:
+  
+  Process error output:
+  node:internal/modules/cjs/loader:1051
+    throw err;
+    ^
+  
+  Error: Cannot find module '/Users/selmahaoual/Memora/.next/webpack-loaders.js'
+      at Module._resolveFilename (node:internal/modules/cjs/loader:1048:15)
+      at Module._load (node:internal/modules/cjs/loader:901:27)
+      at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:83:12)
+      at node:internal/main/run_main_module:23:47 {
+    code: 'MODULE_NOT_FOUND',
+  }
+  
+  Node.js v20.9.0`);
+e.code = 'MODULE_UNPARSEABLE';
+throw e;}}),
+
+};
---
+++ .next/static/chunks/selmahaoual_Memora_src_app_layout_tsx_bf14bc7a._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/c24c7_next_dist_c2e3efa2._.js"
+],
+    source: "dynamic"
+});
